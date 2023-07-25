2c5b83eadeb990362fb901523ed5822f237246f8
Jeferson <jeferson.santos@globalhitss.com.br>
2023-07-24 22:40:24 -0300

    Ajuste codigo

--- src/main/java/br/com/bruno/cadastro/services/impl/UsersServiceImpl.java
+++ src/main/java/br/com/bruno/cadastro/services/impl/UsersServiceImpl.java
@@ -3,25 +3,26 @@ package br.com.bruno.cadastro.services.impl;
 import br.com.bruno.cadastro.domain.UsersEntity;
 import br.com.bruno.cadastro.repository.UserRepository;
 import br.com.bruno.cadastro.services.UsersService;
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.ZonedDateTime;
 
 @Service
 
 public class UsersServiceImpl implements UsersService {
 
-    @Autowired
-    UserRepository repository;
+
+    private final UserRepository repository;
+
+    public UsersServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public UsersEntity saveUsers(UsersEntity entidade){
         entidade.setCreate_at(LocalDate.now().toString());
-        var retorno = repository.save(entidade);
-        return retorno;
+        return repository.save(entidade);
+
     }
 
     @Override
@@ -30,8 +31,8 @@ public class UsersServiceImpl implements UsersService {
       if (findId.isPresent()){
           findId.get().setNome(entidade.getNome());
           findId.get().setUpdate_at(LocalDate.now().toString());
-          var retorno = repository.save(findId.get());
-          return retorno;
+          return repository.save(findId.get());
+
 
       }
       throw new RuntimeException("Usuário não encontrado");
@@ -43,8 +44,7 @@ public class UsersServiceImpl implements UsersService {
       if(findId.isPresent()){
           repository.deleteById(id);
 
-      }
-     else{ throw new RuntimeException("Usuário não encontrado");}
+      }else{ throw new RuntimeException("Usuário não encontrado");}
     }
 
 
