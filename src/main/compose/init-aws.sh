04389a28c753280906fa934f60183e758c6b0b8b
Jeferson <jeferson.santos@globalhitss.com.br>
2023-07-25 00:14:13 -0300

    Ajuste in Docker-compose to run scrit init-aws.sh

--- src/main/compose/init-aws.sh
+++ src/main/compose/init-aws.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 awslocal dynamodb create-table --table-name Users --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
 awslocal dynamodb create-table --table-name Endereco --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
