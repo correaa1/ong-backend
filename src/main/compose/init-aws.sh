5d406f36118e24de0e30595d906922ff4de86765
Jeferson <jeferson.santos@globalhitss.com.br>
2023-08-08 17:33:13 -0300

    Criação da Tebela

--- src/main/compose/init-aws.sh
+++ src/main/compose/init-aws.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 awslocal dynamodb create-table --table-name Users --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+awslocal dynamodb create-table --table-name Delivery --attribute-definitions AttributeName=idDelivery,AttributeType=S --key-schema AttributeName=idDelivery,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
